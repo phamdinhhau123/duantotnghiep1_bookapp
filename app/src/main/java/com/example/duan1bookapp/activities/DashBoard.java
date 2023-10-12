fdc3df2bd7d97fba612098ab4d6ef2b134ca3b4f
anphu123 <anphu221002@gmail.com>
2023-10-12 22:21:31 +0700

    done navigation bottom
    đã hoàn thành bottom navigation

--- app/src/main/java/com/example/duan1bookapp/activities/DashBoard.java
+++ app/src/main/java/com/example/duan1bookapp/activities/DashBoard.java
@@ -1,96 +1,48 @@
 package com.example.duan1bookapp.activities;
 
-import androidx.annotation.NonNull;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.viewpager.widget.ViewPager;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-
+import com.example.duan1bookapp.MangaFragment;
+import com.example.duan1bookapp.MenuFragment;
+import com.example.duan1bookapp.PersonFragment;
 import com.example.duan1bookapp.R;
 import com.example.duan1bookapp.databinding.ActivityDashBoardBinding;
-import com.example.duan1bookapp.databinding.ActivityDashboardUserBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
-
-import java.util.ArrayList;
 
 public class DashBoard extends AppCompatActivity {
 
-    private ActivityDashBoardBinding binding;
+    ActivityDashBoardBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityDashBoardBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
-        setUpViewPager();
-
-        binding.bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int itemId=item.getItemId();
-                switch (itemId){
-                    case R.id.ic_home:
-                        binding.viewpager.setCurrentItem(0);
-                        break;
-                    case R.id.ic_manga:
-                        binding.viewpager.setCurrentItem(1);
-                        break;
-                    case R.id.ic_menubook:
-                        binding.viewpager.setCurrentItem(2);
-                        break;
-                    case R.id.ic_user:
-                        binding.viewpager.setCurrentItem(3);
-                        break;
-                }
-                return true;
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.ic_home) {
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.ic_manga) {
+                replaceFragment(new MangaFragment());
+            } else if (item.getItemId() == R.id.ic_menubook) {
+                replaceFragment(new MenuFragment());
+            } else if (item.getItemId() == R.id.ic_user) {
+                replaceFragment(new PersonFragment());
             }
+            return true;
         });
 
     }
-
-    private void setUpViewPager(){
-        DashboardUserActivity.ViewPagerAdapter viewPagerAdapter=new DashboardUserActivity.ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        binding.viewpager.setAdapter(viewPagerAdapter);
-
-
-
-        binding.viewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                switch (position){
-                    case 0:
-                        binding.bottomNavigation.getMenu().findItem(R.id.ic_home).setChecked(true);
-                        break;
-                    case 1:
-                        binding.bottomNavigation.getMenu().findItem(R.id.ic_manga).setChecked(true);
-                        break;
-                    case 2:
-                        binding.bottomNavigation.getMenu().findItem(R.id.ic_menubook).setChecked(true);
-                        break;
-                    case 3:
-                        binding.bottomNavigation.getMenu().findItem(R.id.ic_user).setChecked(true);
-                        break;
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager= getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
 
