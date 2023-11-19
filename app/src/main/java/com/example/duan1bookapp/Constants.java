5e9375c4cf1239fa8d577299e623d9c97bd3c78b
Dongocthach <sheepold2013@gmail.com>
2023-11-19 21:09:41 +0700

    1234

--- app/src/main/java/com/example/duan1bookapp/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.duan1bookapp;
-
-public class Constants {
-    public static final long MAX_BYTES_PDF= 50000000;//50MB
-
-}
--- app/src/main/java/com/example/duan1bookapp/a_interface/IClickItemChapterLinstener.java
+++
@@ -1,8 +0,0 @@
-package com.example.duan1bookapp.a_interface;
-
-import com.example.duan1bookapp.models.Chapter;
-import com.example.duan1bookapp.models.Product;
-
-public interface IClickItemChapterLinstener {
-    void onClickItemChapter(Chapter Chapter);
-}
